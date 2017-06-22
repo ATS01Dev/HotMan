72212fbce95101185e9992b2e1330eb11e111bdc
ats01 <ats01groupe@gmail.com>
2017-06-22 23:13:50 +0100

    durer

--- src/main/java/bj/ats/hm/web/rest/ClientResource.java
+++ src/main/java/bj/ats/hm/web/rest/ClientResource.java
@@ -20,6 +20,8 @@ import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import java.time.Duration;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.StreamSupport;
@@ -57,7 +59,11 @@ public class ClientResource {
         if (client.getId() != null) {
             return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "idexists", "A new client cannot already have an ID")).body(null);
         }
-        Client result = clientService.save(client);
+//        Date depart = (Date) client.getDatecome();
+//        Date retour= (Date) date_ret.getDate();
+//        long dureest= (depart.getTime()- retour.getTime())/86400000 ;
+        Duration d = Duration.between( client.getDatecome() , client.getDatego() );
+        Client result = clientService.save(client.duration((double) d.toDays()));
         return ResponseEntity.created(new URI("/api/clients/" + result.getId()))
             .headers(HeaderUtil.createEntityCreationAlert(ENTITY_NAME, result.getId().toString()))
             .body(result);
