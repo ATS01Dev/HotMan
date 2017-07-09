bc023976c861cba66603f31172d0c4b51d581543
LCherubin <lcherubin7@gmail.com>
2017-07-09 16:22:14 +0100

    Translation
    French Translation Perform

--- src/main/webapp/app/admin/admin.module.ts
+++ src/main/webapp/app/admin/admin.module.ts
@@ -2,7 +2,9 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { HotManSharedModule } from '../shared';
+
 /* jhipster-needle-add-admin-module-import - JHipster will add admin modules imports here */
+
 
 import {
     adminState,
